4b44f76e55a02b57c7fafc407261b588376494d1
Julien Berard <jujugrrr@gmail.com>
2014-12-11 14:34:20 +0000

    Added comment on Stack integration

--- recipes/apache_mysql_3_vhosts.rb
+++ recipes/apache_mysql_3_vhosts.rb
@@ -22,8 +22,12 @@ listen_ports.each do |port|
   node.default['phpstack']['apache']['sites'][port]['my_site_' + port]['loglevel']       = 'warn'
   node.default['phpstack']['apache']['sites'][port]['my_site_' + port]['server_admin']   = 'demo@demo.com'
   # 1. I cannot add VARS to pass them to my template, I need to change the templates and use attributes inside my templates
-  # 2. the port param is created from the port key, it's not the default server_port like in the web_app definition
+  # 2. the port param is created from the port key, it's not the default server_port like in the web_app definition, making it a bit confusing when adding your own template
   # 3. the site name(vhost filename) is based on the port, if I want to create a loop like above I need a different site name, the only difference is the port so I picked ['my_site_' + port], unfortunatelly it will create my_site_8080-8080.conf. So If I want a clean my_site_8080 I need to define the attributes 3 times.
+  # 4. it takes a while because we have to understand how the stack will use attributes/hash_keys (docroot, config filename, template params) in order to achieve what you want. It should just speak by itself when you read it. 
+  # 5. if the converge fails then the mysql_root_password is not saved therefore the second run root fails to connect to update the users
+  # 6. it's hard ot know which attribute is required/optional/dependent
+  # 7. there is PHP even if I don't want it.
 end
 
 include_recipe 'phpstack::apache'
